804cbf6a40841c9ff4e709476ec49c22ff15e0ca
trofinn <trofin.nicu98@gmail.com>
2023-06-18 23:25:04 +0200

    add new chatroom

--- routes/chatRoute.js
+++ routes/chatRoute.js
@@ -1,4 +1,47 @@
 const router = require("express").Router();
 const Chat = require("../model/chatModel");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 // create new chat
+router.post("/create-new-chat", authMiddleware, async (req, res) => {
+    try {
+        const newChat = new Chat(req.body);
+        const savedChat = await newChat.save();
+        res.send({
+            success: true,
+            message: "Chat created",
+            data: savedChat,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: "Error creating chat",
+            error: error.message,
+        });
+    }
+});
+
+// get all chat of user
+router.get("/get-all-chats", authMiddleware, async (req, res) => {
+    try {
+        const chats = await Chat.find({
+            members:
+            {
+                $in: [req.body.userId],
+            },
+        });
+        res.send({
+            success: true,
+            message: "Chats fetched successfully",
+            data: chats,
+        });
+    } catch (error) {
+        res.send({
+            success: false,
+            message: "Error fetching chat",
+            error: error.message,
+        });
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const dbConfig = require("./config/dbConfig");
 const port = process.env.PORT || 5000;
 
 const usersRoute = require("./routes/usersRoute");
+const chatsRoute = require("./routes/chatRoute");
 app.use(express.json());
 
 app.use("/api/users", usersRoute);
+app.use("/api/chats", chatsRoute);
 
 
 app.listen(port, () => console.log(`Server ok running on port ${port}`));
